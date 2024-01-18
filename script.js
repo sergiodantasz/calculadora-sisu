8ea33adbcedb05d64561c2dce5e7bbe8639c4e94
Sérgio Dantas <sergiodnts828@gmail.com>
2024-01-18 16:58:09 -0300

    feat: implement feature to calculate average

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  calculateAverage()
+})
+
+function calculateAverage() {
+  const averageElement = document.querySelector('.average')
+  const grades = document.querySelector('.grades')
+  const weights = document.querySelector('.weights')
+  let sumGrades = 0.0
+  let sumWeights = 0.0
+  for (let i = 0; i <= 4; i++) {
+    let gradeValue = parseFloat(grades.elements[i].value)
+    let weightValue = parseFloat(weights.elements[i].value)
+    console.log(gradeValue, weightValue)
+    sumGrades += gradeValue * weightValue
+    sumWeights += weightValue
+    console.log(sumWeights)
+  }
+  average = (sumGrades / sumWeights).toFixed(1)
+  averageElement.textContent = average
+}
